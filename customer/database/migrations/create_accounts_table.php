9ae73daade284edaaed385169c9794bda2dcdfb8
Nazmus shakib <72853962+Nazmus-shakib-1@users.noreply.github.com>
2021-01-12 22:51:38 +0600

    commit
    laravel project

--- customer/database/migrations/create_accounts_table.php
+++ customer/database/migrations/create_accounts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCustomerssTable extends Migration
+class CreateAccountsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateCustomerssTable extends Migration
      */
     public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('accounts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('password');
+            $table->text('detail');
             $table->timestamps();
         });
     }
-
+  
     /**
      * Reverse the migrations.
      *
@@ -28,6 +28,6 @@ class CreateCustomerssTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('accounts');
     }
 }
